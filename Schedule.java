5db983f8e6ad267f549bd3631ebfd05bb5556b8c
Kydo Solis <kdvsolis@gmail.com>
2019-05-05 14:57:15 +0800

    Initial commit

---
+++ Schedule.java
@@ -0,0 +1,116 @@
+package interview;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class Schedule {
+    private List<Date> byList = new ArrayList<Date>();
+
+    private boolean everyDay;
+
+    private boolean everyHour;
+
+    private List<String> everyList = new ArrayList<String>();
+
+    private String scheduleString;
+
+    public Schedule(String scheduleString) {
+        this.scheduleString = scheduleString;
+        this.everyDay = this.scheduleString.toLowerCase().contains("every day") || this.scheduleString.toLowerCase().contains("sun-sat");
+        this.everyHour = this.scheduleString.toLowerCase().contains("every hour");
+    }
+
+    public boolean hasBy() {
+        return scheduleString.trim().toLowerCase().startsWith("by ");
+    }
+
+    public List<Date> getByList() {
+        byList.clear();
+        if(this.scheduleString.contains("by")) {
+            String time = this.scheduleString.substring(this.scheduleString.indexOf("by") + 2, this.scheduleString.indexOf("every"));
+            String [] timeArray = time.split(",");
+            for(int i = 0; i < timeArray.length; i++) {
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm a z");
+                    dateFormat.setTimeZone(TimeZone.getTimeZone("EST"));
+                    byList.add(dateFormat.parse(timeArray[i].trim()));
+                } catch (ParseException ex) {
+                    Logger.getLogger(Schedule.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return this.byList;
+    }
+
+    public boolean isEveryDay() {
+        return this.everyDay;
+    }
+
+    public boolean isEveryHour() {
+        return this.everyHour;
+    }
+
+    public List<String> getEveryList() {
+        everyList.clear();
+        String []allWeekdays = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        String allDays = "1st,2nd,3rd,4th,5th,6th,7th,8th,9th,10th,11th,12th,13th,14th,15th,16th,17th,18th,19th,20th,21st,22nd,23rd,24th,25th,26th,27th,28th,29th,30th,31st";
+        String []allDaysArray = allDays.split(",");
+        if(!this.everyDay) {
+            if(StringUtils.indexOfAny(this.scheduleString, allWeekdays) > -1 || 
+               this.scheduleString.substring(this.scheduleString.indexOf("every") + 6).contains("hour")|| 
+               this.scheduleString.substring(this.scheduleString.indexOf("every") + 6).contains("minute")|| 
+               this.scheduleString.substring(this.scheduleString.indexOf("every") + 6).contains("seconds")) {
+                String daysOrHours = this.scheduleString.substring(this.scheduleString.indexOf("every") + 6);
+                if(daysOrHours.contains("-")) {
+                   String start = daysOrHours.split("-")[0].trim();
+                   String end = daysOrHours.split("-")[1].trim();
+                   int i = 0;
+                   while(!allWeekdays[i].toLowerCase().equals(start.toLowerCase()) && i < 7) {
+                       i++;
+                   }
+                   while(!allWeekdays[i].toLowerCase().equals(end.toLowerCase()) && i < 7) {
+                       everyList.add(allWeekdays[i++]);
+                   } 
+                   everyList.add(end);
+                } else if (daysOrHours.replace(" ", "").contains(",")) {
+                    everyList =  Arrays.asList(daysOrHours.split(", "));
+                } else if(!this.everyHour) {
+                    everyList.add(daysOrHours);
+                }
+            }
+            if(StringUtils.indexOfAny(this.scheduleString, allDaysArray) > -1) {
+                String numberedDate = this.scheduleString.substring(this.scheduleString.indexOf("every") + 6);
+                if(numberedDate.contains("-")) {           
+                   String start = numberedDate.split("-")[0].trim();
+                   String end = numberedDate.split("-")[1].trim();
+                   int i = 0;
+                   while(!allDaysArray[i].toLowerCase().equals(start.toLowerCase())) {
+                       i = (i + 1) % allDaysArray.length;
+                   }
+                   while(!allDaysArray[i].toLowerCase().equals(end.toLowerCase())) {
+                       everyList.add(allDaysArray[i]);
+                       i = (i + 1) % allDaysArray.length;
+                   } 
+                   everyList.add(end);
+                } else if (numberedDate.replace(" ", "").contains(",")) {
+                    everyList =  Arrays.asList(numberedDate.split(", "));
+                } else {
+                    everyList.add(numberedDate.trim());
+                }
+            }
+        }
+          
+        return everyList;
+    }
+}
---
+++ ScheduleTest.java
@@ -0,0 +1,147 @@
+package interview;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.junit.Test;
+
+public class ScheduleTest {
+    @Test
+    public void testEveryDay() {
+        Schedule schedule = new Schedule("every day");
+        assertSchedule(schedule, false, true, false, null, null);
+    }
+
+    @Test
+    public void testEveryMonToFri() {
+        Schedule schedule = new Schedule("every Mon-Fri");
+        assertSchedule(schedule, false, false, false, null, "Mon,Tue,Wed,Thu,Fri");
+    }
+
+    @Test
+    public void testEveryMonWedFri() {
+        Schedule schedule = new Schedule("every Mon, Wed, Fri");
+        assertSchedule(schedule, false, false, false, null, "Mon,Wed,Fri");
+    }
+
+    @Test
+    public void testEveryHour() {
+        Schedule schedule = new Schedule("every hour");
+        assertSchedule(schedule, false, false, true, null, null);
+    }
+
+    @Test
+    public void testEvery6Hours() {
+        Schedule schedule = new Schedule("every 6 hours");
+        assertSchedule(schedule, false, false, false, null, "6 hours");
+    }
+
+    @Test
+    public void testEvery30Minutes() {
+        Schedule schedule = new Schedule("every 30 minutes");
+        assertSchedule(schedule, false, false, false, null, "30 minutes");
+    }
+
+    @Test
+    public void testBy1AmEveryDay() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every day");
+        assertSchedule(schedule, true, true, false, "1:00 AM EST", null);
+
+    }
+
+    @Test
+    public void testBy1AmEveryFri() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every Fri");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "Fri");
+    }
+
+    @Test
+    public void testBy1AmEveryMonToFri() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every Mon-Fri");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "Mon,Tue,Wed,Thu,Fri");
+    }
+
+    @Test
+    public void testBy1AmEveryMonWedFri() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every Mon, Wed, Fri");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "Mon,Wed,Fri");
+    }
+
+    @Test
+    public void testBy1AmEvery1st() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every 1st");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "1st");
+    }
+
+    @Test
+    public void testBy1AmEvery15th30th() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every 15th, 30th");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "15th,30th");
+    }
+
+    @Test
+    public void testBy1AmEvery10thTo12th() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every 10th-12th");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "10th,11th,12th");
+    }
+
+    @Test
+    public void testBy1AmEvery31stTo3rd() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every 31st-3rd");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "31st,1st,2nd,3rd");
+    }
+
+    @Test
+    public void testBy1AmEvery2ndTo22nd() {
+        Schedule schedule = new Schedule("by 1:00 AM EST every 2nd-22nd");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST",
+                "2nd,3rd,4th,5th,6th,7th,8th,9th,10th,11th,12th,13th,14th,15th,16th,17th,18th,19th,20th,21st,22nd");
+    }
+
+    @Test
+    public void testBy9Am5PmEverySunToFri() {
+        Schedule schedule = new Schedule("by 9:00 AM EST, 5:00 PM EST every Sun-Fri");
+        assertSchedule(schedule, true, false, false, "9:00 AM EST,5:00 PM EST", "Sun,Mon,Tue,Wed,Thu,Fri");
+    }
+
+    @Test
+    public void testBy9Am5PmEveryMonToFri() {
+        Schedule schedule = new Schedule("by 9:00 AM EST, 5:00 PM EST every Mon-Fri");
+        assertSchedule(schedule, true, false, false, "9:00 AM EST,5:00 PM EST", "Mon,Tue,Wed,Thu,Fri");
+    }
+
+    private void assertSchedule(Schedule schedule, boolean hasBy, boolean isEveryDay, boolean isEveryHour,
+            String expectedByString, String expectedEveryString) {
+        assertEquals(hasBy, schedule.hasBy());
+        assertEquals(isEveryDay, schedule.isEveryDay());
+        assertEquals(isEveryHour, schedule.isEveryHour());
+        assertDateList(schedule.getByList(), expectedByString);
+        assertList(schedule.getEveryList(), expectedEveryString);
+    }
+
+    private void assertList(List<String> list, String string) {
+        String[] split = new String[0];
+        if (string != null) {
+            split = string.split(",");
+        }
+
+        String[] everyArray = list.toArray(new String[0]);
+        assertTrue(ArrayUtils.isEquals(split, everyArray));
+    }
+
+    private void assertDateList(List<Date> dates, String string) {
+        List<String> list = new ArrayList<String>();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm a z");
+        for (Date date : dates) {
+            list.add(dateFormat.format(date));
+        }
+
+        assertList(list, string);
+    }
+}
